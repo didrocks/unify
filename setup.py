8f9e97aabd0372e0a6c7f6665d2b51f2eb0a840a
Didier Roche <didier.roche@canonical.com>
2011-02-19 10:39:17 +0100

    Initial project creation with Quickly!

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+###################### DO NOT TOUCH THIS (HEAD TO THE SECOND PART) ######################
+
+import os
+import sys
+
+try:
+    import DistUtilsExtra.auto
+except ImportError:
+    print >> sys.stderr, 'To build unify you need https://launchpad.net/python-distutils-extra'
+    sys.exit(1)
+assert DistUtilsExtra.auto.__version__ >= '2.18', 'needs DistUtilsExtra.auto >= 2.18'
+
+def update_data_path(prefix, oldvalue=None):
+
+    try:
+        fin = file('unify/unifyconfig.py', 'r')
+        fout = file(fin.name + '.new', 'w')
+
+        for line in fin:            
+            fields = line.split(' = ') # Separate variable from value
+            if fields[0] == '__unify_data_directory__':
+                # update to prefix, store oldvalue
+                if not oldvalue:
+                    oldvalue = fields[1]
+                    line = "%s = '%s'\n" % (fields[0], prefix)
+                else: # restore oldvalue
+                    line = "%s = %s" % (fields[0], oldvalue)
+            fout.write(line)
+
+        fout.flush()
+        fout.close()
+        fin.close()
+        os.rename(fout.name, fin.name)
+    except (OSError, IOError), e:
+        print ("ERROR: Can't find unify/unifyconfig.py")
+        sys.exit(1)
+    return oldvalue
+
+
+class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
+    def run(self):
+        previous_value = update_data_path(self.prefix + '/share/unify/')
+        DistUtilsExtra.auto.install_auto.run(self)
+        update_data_path(self.prefix, previous_value)
+
+
+        
+##################################################################################
+###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
+##################################################################################
+
+DistUtilsExtra.auto.setup(
+    name='unify',
+    version='0.1',
+    #license='GPL-3',
+    #author='Your Name',
+    #author_email='email@ubuntu.com',
+    #description='UI for managing …',
+    #long_description='Here a longer description',
+    #url='https://launchpad.net/unify',
+    cmdclass={'install': InstallAndUpdateDataDirectory}
+    )
+
